40372502027500fd1ae21ff58d1ccbb6d5358e07
Andrei Manolache <andrei_mano@outlook.com>
2021-11-23 22:12:26 +0000

    added removed_entities param
    Signed-off-by: Andrei Manolache <andrei_mano@outlook.com>

--- utils/eliminate_ne.py
+++ utils/eliminate_ne.py
@@ -31,7 +31,9 @@ def remove_named_entities(text: str, entity_subset: list) -> str:
     return text
 
 
-def remove_entities_from_json(path: str, spacy_ner_f=en_core_web_trf) -> dict:
+def remove_entities_from_json(
+    path: str, removed_entities=["PERSON"], spacy_ner_f=en_core_web_trf
+) -> dict:
     spacy_ner_f = spacy_ner_f.load()
     dataset = json.load(open(path))
 
@@ -41,8 +43,8 @@ def remove_entities_from_json(path: str, spacy_ner_f=en_core_web_trf) -> dict:
     ne_t1 = get_named_entities(text_a1, spacy_ner_f)
     ne_t2 = get_named_entities(text_a2, spacy_ner_f)
 
-    entities_to_be_removed_t1 = get_entity_subset(ne_t1, ["PERSON"])
-    entities_to_be_removed_t2 = get_entity_subset(ne_t2, ["PERSON"])
+    entities_to_be_removed_t1 = get_entity_subset(ne_t1, removed_entities)
+    entities_to_be_removed_t2 = get_entity_subset(ne_t2, removed_entities)
 
     dataset["pair"][0] = remove_named_entities(text_a1, entities_to_be_removed_t1)
     dataset["pair"][1] = remove_named_entities(text_a2, entities_to_be_removed_t2)
