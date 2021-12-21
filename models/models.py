eab732006d0faf4959e930cecaaef561dfa5b9d9
Andrei Manolache <andrei_mano@outlook.com>
2021-12-21 06:02:14 +0000

    added clf model __call__
    Signed-off-by: Andrei Manolache <andrei_mano@outlook.com>

--- models/models.py
+++ models/models.py
@@ -1,5 +1,12 @@
-from transformers import AutoModelForMaskedLM, AutoModelForSequenceClassification, AutoModel, AutoTokenizer
+from transformers import (
+    AutoModelForMaskedLM,
+    AutoModelForSequenceClassification,
+    AutoModel,
+    AutoTokenizer,
+)
 import torch
+from torch.nn.functional import softmax
+
 
 class TrainableModel:
     def __init__(
@@ -11,12 +18,14 @@ class TrainableModel:
     ):
         self.model = model_type.from_pretrained(model_name, **kwargs).to(device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, **kwargs)
+        self.device = device
 
     def __call__(self, input_data):
         return self.model(**input_data)
 
     def save_pretrained(self, path):
-        self.model.save_pretrained(path)    
+        self.model.save_pretrained(path)
+
 
 class TrainablePromptModel(TrainableModel):
     def __init__(
@@ -27,7 +36,7 @@ class TrainablePromptModel(TrainableModel):
         **kwargs
     ):
         super().__init__(model_name, model_type, device, **kwargs)
-        
+
 
 class TrainableClfModel(TrainableModel):
     def __init__(
@@ -38,4 +47,18 @@ class TrainableClfModel(TrainableModel):
         **kwargs
     ):
         super().__init__(model_name, model_type, device, **kwargs)
-        
+
+    def __call__(self, input_data):
+        batch_iid = input_data[0]
+        batch_am = input_data[1]
+        batch_tti = input_data[2]
+
+        return softmax(
+            self.model(
+                {
+                    "input_ids": batch_iid.to(self.device),
+                    "attention_mask": batch_am.to(self.device),
+                    "token_type_ids": batch_tti.to(self.device),
+                }
+            )[0]
+        )
