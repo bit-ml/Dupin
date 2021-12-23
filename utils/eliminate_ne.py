567659e4a877ee933c3e5839fb5d197987ebe76e
fbrad <florin.brad42@gmail.com>
2021-12-23 12:25:53 +0000

    Updated eliminate_ne to work with .jsonl

--- utils/eliminate_ne.py
+++ utils/eliminate_ne.py
@@ -2,8 +2,13 @@
 import en_core_web_trf
 import json
 import os
+import time
 from pathlib import Path
 from spacy import displacy
+from typing import Dict
+import spacy
+spacy.require_gpu()
+#nlp = spacy.load('en_core_web_md')
 
 
 def get_named_entities(text: str, spacy_ner_f) -> list:
@@ -34,46 +39,65 @@ def remove_named_entities(text: str, entity_subset: list) -> str:
 
 
 def remove_entities_from_json(
-    path: str, removed_entities=["PERSON"], spacy_ner_f=en_core_web_trf
+    sample: Dict, spacy_ner_f, removed_entities=["PERSON"], 
 ) -> dict:
-    spacy_ner_f = spacy_ner_f.load()
-    sample = json.load(open(path))
+    #spacy_ner_f = spacy_ner_f.load()
 
     text_a1 = sample["pair"][0]
     text_a2 = sample["pair"][1]
 
+    tic1 = time.perf_counter()
     ne_t1 = get_named_entities(text_a1, spacy_ner_f)
     ne_t2 = get_named_entities(text_a2, spacy_ner_f)
 
+    tic2 = time.perf_counter()
     entities_to_be_removed_t1 = get_entity_subset(ne_t1, removed_entities)
     entities_to_be_removed_t2 = get_entity_subset(ne_t2, removed_entities)
 
+    tic3 = time.perf_counter()
     sample["pair"][0] = remove_named_entities(text_a1, entities_to_be_removed_t1)
     sample["pair"][1] = remove_named_entities(text_a2, entities_to_be_removed_t2)
+    tic4 = time.perf_counter()
+
+    print("time of get_named_entities(): ", (tic2-tic1))
+    print("time of get_entity_subset(): ", (tic3-tic2))
+    print("time of remove_named_entities(): ", (tic4-tic3))
 
     return sample
 
 
 def main():
-    modes = ["train", "test"]
-    root = "/darkweb_ds/"
-    subset_type = "open_splits/unseen_authors/xs/"
+    modes = ["train", "val", "test"]
+    # root = "/darkweb_ds/"
+    # subset_type = "open_splits/unseen_authors/xs/"
+    root = "/pan2020"
+    subset_type = "open_splits/unseen_all/xs"
+    spacy_ner_f = spacy.load('en_core_web_trf')
 
     for mode in modes:
         print(f"[processing] Mode: {mode}")
-        subset_mode = f"/pan20-av-small-{mode}/"
-        removed_ne_path = f"/pan20-av-small-noauthors-{mode}"
+        subset_mode = f"pan20-av-small-{mode}.jsonl"
+        removed_ne_path = f"pan20-av-small-noauthors-{mode}.jsonl"
 
         ds_path_full = os.path.join(root, subset_type, subset_mode)
         ds_path_removed_ne = os.path.join(root, subset_type, removed_ne_path)
+        print("ds path: ", ds_path_full)
+        print("target path: ", ds_path_removed_ne)
 
-        Path(ds_path_removed_ne).mkdir(parents=True, exist_ok=True)
+        #Path(ds_path_removed_ne).mkdir(parents=True, exist_ok=True)
+        new_samples = []
+        if ds_path_full.endswith('.jsonl'):
+            with open(ds_path_full) as f:
+                for idx, line in enumerate(f):
+                    print("processed ", idx, " examples")
+                    sample = json.loads(line)
+                    new_samples.append(remove_entities_from_json(
+                        sample=sample, 
+                        spacy_ner_f=spacy_ner_f
+                    ))
 
-        for fname in os.listdir(ds_path_full):
-            if fname.endswith(".json"):
-                sample = remove_entities_from_json(fname)
-                with open(os.path.join(ds_path_removed_ne, fname)) as json_fp:
-                    json.dump(sample, json_fp, indent=2)
+        with open(ds_path_removed_ne) as f:
+            json.dump(new_samples, f, indent=2)
 
 main()
 # %%
